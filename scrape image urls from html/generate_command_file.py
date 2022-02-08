00f1f7049f7b729ea9d8f8f4ce3583390f055711
Glitched-Manual <popcorneater720@gmail.com>
2022-02-08 01:00:51 -0500

    added comadline option for command gen

--- scrape image urls from html/generate_command_file.py
+++ scrape image urls from html/generate_command_file.py
@@ -2,43 +2,75 @@
 import os
 import sys
 
-print("enter folder path")
+directory_arg_position = None
+arg_count = 0
+directory_path = None
+directory_arg_passed = False
 
-path_to_folder = None
 
-try:
-	path_to_folder = str(input())
-except:
-	print(sys.exc_info())
+def write_command_file(passed_directory):    
+    if passed_directory:
+        if passed_directory.endswith('/') is False:
+            passed_directory = passed_directory + '/'
 
-if path_to_folder:
-    if path_to_folder.endswith('/') is False:
-        path_to_folder = path_to_folder + '/'
-
-    # if path starts with ./
-    #
-    if path_to_folder.startswith("./"):
-        terminal_pwd = os.getcwd()
-        path_to_folder.replace("./",terminal_pwd)
+        # if path starts with ./
+        #
+        if passed_directory.startswith("./"):
+            terminal_pwd = os.getcwd()
+            passed_directory.replace("./",terminal_pwd)
 
 
 
-    #get filenames
+        #get filenames
+
+        file_list = os.listdir(passed_directory + "html_pages")
+
+        # open command file
+
+        f = open("./my_command.txt","w")
+
+        for target_file in file_list:
+            #trying \" to allow for spaces
+            command = "er_media_pull -f \"" + str(passed_directory) + "\"/html_pages/\"" + str(target_file) + "\" -d " + str(passed_directory)
+            f.write(command + "\n")
+            
+        f.close()
+    else:
+        print("error passed_directory is null")
+
+args_length = len(sys.argv)
+
+for arg in sys.argv:
+    if arg == "-d":
+        directory_arg_position = arg_count
+        directory_arg_passed = True
+        print("directory found")
+        if args_length > directory_arg_position + 1:
+            directory_path = sys.argv[directory_arg_position + 1]
 
 
+    arg_count = arg_count + 1     
 
+if directory_arg_passed:
 
-    file_list = os.listdir(path_to_folder + "html_pages")
+    print("yay")
+    if directory_path:
 
-    # open command file
+        write_command_file(directory_path)
 
-    f = open("./my_command.txt","w")
-
-    for target_file in file_list:
-        #trying \" to allow for spaces
-        command = "er_media_pull -f \"" + str(path_to_folder) + "\"/html_pages/\"" + str(target_file) + "\" -d " + str(path_to_folder)
-        f.write(command + "\n")
-        
-    f.close()
 else:
-    print("error path_to_folder is null")
+
+
+
+    # if proper args not given do below
+
+    print("enter folder path")
+
+    path_to_folder = None
+
+    try:
+        path_to_folder = str(input())
+    except:
+        print(sys.exc_info())
+
+    write_command_file(path_to_folder)
